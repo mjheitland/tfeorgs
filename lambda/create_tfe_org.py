55735eaa1ec32e3ee694e05ef58c927f8e12f8a7
Michael Heitland <heitlm@amazon.com>
2020-11-24 16:56:27 +0100

    adding tfe_api_token

--- lambda/create_tfe_org.py
+++ lambda/create_tfe_org.py
@@ -2,6 +2,7 @@ from botocore.exceptions import ClientError
 import boto3
 import json
 import logging
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,6 +14,12 @@ def lambda_handler(event, context):
         logger.info("event = {}".format(event))
         app_name = event["app_name"]
         logger.info(f"app_name = {app_name}")
+        user_email = event["user_email"]
+        logger.info(f"user_email = {user_email}")
+        environment = event["environment"]
+        logger.info(f"environment = {environment}")
+        tfe_api_token = os.environ("TFE_API_TOKEN")
+        logger.info(f"tfe_api_token: {tfe_api_token.sub[:10]}")
         logger.info("... finishing create_tfe_org.")
     except ClientError as e:
         logger.error("*** Error in create_tfe_org: {}".format(e))
